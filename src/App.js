2e3554f60756192be163cb8b6f8c91827e7f29ab
shj9206 <shj9206@gmail.com>
2021-01-06 20:34:28 +0900

    <2021.01.06><FirstCommit><shoes shop 만들기 시작><기본 뼈대 만들기>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+/* eslint-disable */
+import React from 'react';
+import {Navbar, Nav, NavDropdown, Button, Jumbotron} from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+      <Navbar bg="light" expand="lg" >
+  <Navbar.Brand href="#home">Shoos Shop</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="ml-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+
+<Jumbotron className="background">
+  <h1>Welcome to Song's Shop</h1>
+  <p>
+    This is a simple hero unit, a simple jumbotron-style component for calling
+    extra attention to featured content or information.
+  </p>
+  <p>
+    <Button variant="primary">Learn more</Button>
+  </p>
+</Jumbotron>
+
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4">
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg"
+          width="100%"/>
+          <h4>상품명</h4>
+          <p>상품설명 & 가격</p>
+        </div>
+        <div className="col-md-4">
+          <img src="https://codingapple1.github.io/shop/shoes2.jpg"
+          width="100%"/>
+          <h4>상품명</h4>
+          <p>상품설명 & 가격</p>
+        </div>
+        <div className="col-md-4">
+          <img src="https://codingapple1.github.io/shop/shoes3.jpg"
+          width="100%"/>
+          <h4>상품명</h4>
+          <p>상품설명 & 가격</p>
+        </div>
+      </div>
+    </div>
+
     </div>
   );
 }
