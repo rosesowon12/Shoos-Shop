afe0b9b15e21bd0d534ee29407cb9bbf76fcee54
shj9206 <shj9206@gmail.com>
2021-01-07 19:33:19 +0900

    <2021.01.07><secondCommit>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 /* eslint-disable */
-import React from 'react';
+import React, {useState} from 'react';
 import {Navbar, Nav, NavDropdown, Button, Jumbotron} from 'react-bootstrap';
 import './App.css';
+import Data from './Data.js';
+import { Link, Route, Switch } from 'react-router-dom';
+import Detail from './Detail.js';
 
 function App() {
+
+  let [shoes, shoes변경] = useState(Data);
+
   return (
     <div className="App">
       
@@ -12,9 +18,9 @@ function App() {
   <Navbar.Brand href="#home">Shoos Shop</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="ml-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
+    <Nav className="mr-auto">
+      <Nav.Link><Link to="/">Home</Link></Nav.Link>
+      <Nav.Link><Link to="/detail">Detail</Link></Nav.Link>
       <NavDropdown title="Dropdown" id="basic-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -26,7 +32,9 @@ function App() {
   </Navbar.Collapse>
 </Navbar>
 
-<Jumbotron className="background">
+<Switch>
+<Route exact path="/">
+  <Jumbotron className="background">
   <h1>Welcome to Song's Shop</h1>
   <p>
     This is a simple hero unit, a simple jumbotron-style component for calling
@@ -35,33 +43,43 @@ function App() {
   <p>
     <Button variant="primary">Learn more</Button>
   </p>
-</Jumbotron>
+  </Jumbotron>
 
-    <div className="container">
+  <div className="container">
       <div className="row">
-        <div className="col-md-4">
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg"
-          width="100%"/>
-          <h4>상품명</h4>
-          <p>상품설명 & 가격</p>
-        </div>
-        <div className="col-md-4">
-          <img src="https://codingapple1.github.io/shop/shoes2.jpg"
-          width="100%"/>
-          <h4>상품명</h4>
-          <p>상품설명 & 가격</p>
-        </div>
-        <div className="col-md-4">
-          <img src="https://codingapple1.github.io/shop/shoes3.jpg"
-          width="100%"/>
-          <h4>상품명</h4>
-          <p>상품설명 & 가격</p>
-        </div>
+      { 
+        shoes.map((a,i)=>{
+        return <Card key={i} shoes={shoes[i]} i={i} />
+         })
+      }
       </div>
-    </div>
+  </div>
+</Route>
 
+<Route path="/detail/:id">
+  <Detail shoes={shoes}/>
+</Route>
+
+<Route path="/:id">
+  <div>아무거나</div>
+</Route>
+
+</Switch>
     </div>
-  );
+  )
+}
+
+
+
+
+function Card(props){
+  return (
+    <div className="col-md-4">
+      <img src={ 'https://codingapple1.github.io/shop/shoes' + (props.i+1) + '.jpg' } width="100%"/>
+      <h4>{ props.shoes.title }</h4>
+      <p>{ props.shoes.content } & { props.shoes.price }</p>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/Detail.js
@@ -0,0 +1,38 @@
+/* eslint-disable */
+import React, {useState} from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+
+function Detail(props) {
+
+    let history = useHistory();
+
+    let { id } = useParams();
+    let findDetail = props.shoes.find(function (detail) {
+        return detail.id == id
+    });
+
+    return(
+      <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img src={'https://codingapple1.github.io/shop/shoes'+(findDetail.id+1)+'.jpg'} width="100%" />
+            </div>
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">{findDetail.title}</h4>
+              <p>{findDetail.content}</p>
+              <p>{findDetail.price}원</p>
+              <button className="btn btn-danger">주문하기</button> 
+              <button className="btn btn-danger" onClick={()=>{
+                history.goBack();
+              }}>뒤로가기</button> 
+           
+            </div>
+          </div>
+    </div> 
+  
+    )
+    
+  }
+
+
+  export default Detail;
